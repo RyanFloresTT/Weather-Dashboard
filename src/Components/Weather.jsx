8aa78c6d5ce21264bc736022aa8e812f64941c16
TrustyTea <53247675+TrustyTea@users.noreply.github.com>
2022-10-03 22:31:19 -0700

    Update useEffect to clearInterval

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+
 const API_KEY = process.env.REACT_APP_API_KEY;
 const axios = require("axios");
+const location = "London";
 
 export default function Weather() {
   const [temp, setTemp] = useState(0);
@@ -9,7 +11,7 @@ export default function Weather() {
     try {
       const response = await axios.get(
         "https://api.openweathermap.org/data/2.5/weather",
-        { params: { q: "Cathedral City", units: "imperial", appid: API_KEY } }
+        { params: { q: location, units: "imperial", appid: API_KEY } }
       );
       const currentTemperature = Math.floor(response.data.main.temp);
       setTemp(currentTemperature);
@@ -19,14 +21,18 @@ export default function Weather() {
   }
 
   useEffect(() => {
-    setInterval(() => {
+    const tempInterval = setInterval(() => {
       getWeatherData();
     }, 5000);
-  });
+
+    return () => clearInterval(tempInterval); //This is important
+  }, []);
 
   return (
     <div className="weather">
-      <h1 className="weather-heading">It is currently {temp}°</h1>
+      <h1 className="weather-heading">
+        It is currently {temp}° in {location}.
+      </h1>
     </div>
   );
 }
