24bf506bc6e7ad1f2fa912d112d04c70009c6272
TrustyTea <53247675+TrustyTea@users.noreply.github.com>
2022-10-09 23:03:16 -0700

    Add more styling

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -2,8 +2,8 @@ export default function WeatherCard(props) {
   var pubDate = new Date(props.time * 1000).toDateString();
   return (
     <div className="weather-card">
-      <h1>Date: {pubDate}</h1>
-      <h3>Temp: {props.temperature}</h3>
+      <h1>{pubDate.slice(4, 10)}</h1>
+      <h3>{props.temperature}&deg;</h3>
     </div>
   );
 }
